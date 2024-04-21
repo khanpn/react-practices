cbb4b27a0da24976bc5f8e5e7be0db53e61f9ff2
Khanh Nguyen <pkhanhit@gmail.com>
2024-04-20 21:52:44 -0500

    Stored Color Mode into Local Storage

--- game-hub/src/hooks/useTheme.ts
+++ game-hub/src/hooks/useTheme.ts
@@ -1,12 +1,31 @@
-import { createTheme } from "@mui/material/styles";
-import React from "react";
+import { createTheme } from "@mui/material";
+import React, { useState } from "react";
+
+const getPersistedColorMode = (): "light" | "dark" | undefined => {
+  const persistedValue = localStorage.getItem("colorMode");
+  switch (persistedValue) {
+    case "light":
+      return "light";
+    case "dark":
+      return "dark";
+    default:
+      return undefined;
+  }
+};
 
 export const useTheme = (initialtMode: "light" | "dark" = "light") => {
-  const [mode, setMode] = React.useState<"light" | "dark">(initialtMode);
+  const persistedColorMode = getPersistedColorMode();
+  const [mode, setMode] = useState<"light" | "dark">(
+    persistedColorMode || initialtMode
+  );
   const colorMode = React.useMemo(
     () => ({
       toggleColorMode: () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+        setMode((prevMode) => {
+          const newMode = prevMode === "light" ? "dark" : "light";
+          localStorage.setItem("colorMode", newMode);
+          return newMode;
+        });
       },
     }),
     []
